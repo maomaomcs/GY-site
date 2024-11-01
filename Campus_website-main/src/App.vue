207c659e84fcea7659359beb894c2740707ec244
毛春生 <3150397037@qq.com>
2024-11-01 21:34:08 +0800

    路由问题
    路由问题

--- Campus_website-main/src/App.vue
+++ Campus_website-main/src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
 import index from "@/index.vue";
+import headers from "./view/headers.vue";
+import foot from "./view/foot.vue";
 </script>
 
 <template>
-    <router-link to="/"></router-link>
-    <router-link to="/login"></router-link>
-    <router-view></router-view>
+    <!-- <router-link to="/"></router-link>
+    <router-link to="/login"></router-link> -->
+    <headers></headers>
+    <foot></foot>
+    <!-- <router-view></router-view> -->
 </template>
 
 <style scoped>
--- Campus_website-main/src/index.vue
+++ Campus_website-main/src/index.vue
@@ -1,11 +1,13 @@
 <script setup>
 import page_foot from '@/view/foot.vue'
 import page_head from '@/view/headers.vue'
+import index from './components/index.vue';
 </script>
 
 <template>
-    <page_head></page_head>
-    <page_foot></page_foot>
+    <!-- <page_head></page_head>
+    <page_foot></page_foot> -->
+    <index></index>
     <t-back-top container="body" :visible-height="0" :offset="[20, 0]" style="position: fixed; right: 20px; bottom: 15px"></t-back-top>
 </template>
 
